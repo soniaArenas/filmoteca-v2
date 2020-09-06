be47c67541b0b438acbbcddaa29fdb452cb3eab2
Sonia Arenas <puente46191@gmail.com>
2020-09-06 16:49:30 +0200

    Traducción de la descripción añadida.

--- Controllers/getAllInfo.php
+++ Controllers/getAllInfo.php
@@ -21,18 +21,41 @@ if (isset($_POST["getImg"])) {
     foreach ($linkObjs2 as $linkObj2) {
 	     $descr  = trim($linkObj2->plaintext);
   
-    echo $descr;
 
     } 
-/*
-$textt = $descr;
 
-$json = file_get_contents('https://api.mymemory.translated.net/get?q='.urlencode($textt).'&langpair=en|es');
+ $language_from = 'en';
 
-    if (!empty($json)) { 
-    $obj = json_decode($json);   
-    echo $obj->responseData->translatedText;
-    }*/
+$text_to_translate =$descr;
+
+
+    $language_to = 'es';
+
+
+$encoded_text = urlencode(strip_tags($text_to_translate));
+
+$url = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=' . $language_from . '&tl=' . $language_to . '&dt=t&q=' . $encoded_text;
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+curl_setopt($ch, CURLOPT_USERAGENT, 'AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1');
+$output = curl_exec($ch);
+curl_close($ch);
+
+
+$response_a = json_decode($output);
+
+
+foreach ($response_a[0] as $text_block) {
+    echo  $text_block[0];
+}
+
+
 } else if (isset($_POST["getDirect"])) { 
     $linkObjs2 = $htmlImdb->find('.credit_summary_item a[href$=_dr]');
     foreach ($linkObjs2 as $linkObj2) {
