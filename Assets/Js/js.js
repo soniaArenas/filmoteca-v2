7b7c77aa6a923e1bb9a5958dbc025f0edb8a32f9
Sonia Arenas <puente46191@gmail.com>
2020-09-06 15:43:13 +0200

    Archivo js con todas las funciones necesarias y consultas vía Ajax.

---
+++ Assets/Js/js.js
@@ -0,0 +1,188 @@
+ $(document).ready(function() { 
+
+  loadFilms("name","asc");
+
+  $('#addFilmDiv').hide();
+
+
+  $("#addBtn").click(function() { 
+
+    if ($('#addFilmDiv').is(':visible')) {
+
+
+     $('#addFilmDiv').hide();
+     $('#name, #year').val('');
+
+   } else {
+     $('#addFilmDiv').show();
+   }
+ }); 
+
+
+  $("#setFilm").click(function() { 
+
+   var nameReal = $('#name').val();
+   var name2 = nameReal.toLowerCase();
+   var nameForSearch = name2.split(" ").join("+"); 
+   nameForSearch = nameForSearch+"+imdb";
+
+   var year2 = $('#year').val();
+
+   $.ajax({
+    type: "POST",
+    url: "Controllers/googleSearch.php",
+    data: { str:nameForSearch },
+    success: function(linkImdb){
+
+     $.ajax({
+      type: "POST",
+      url: "Controllers/queryImdb.php",
+      data: { linkImdb: linkImdb },
+      success: function(noteImdb){
+
+        if(name2 != "" && year2 !=""){
+
+         $.ajax({
+          type: "POST",
+          url: "Controllers/getFilmsController.php",
+          data: { name: name2, year: year2, note:noteImdb, link:linkImdb, addFilm:"true" },
+          success: function(data3){
+            console.log(data3);
+            loadFilms("name","asc");
+            $('#name, #year').val('');
+            
+            getAllInfo(linkImdb, nameReal, year2);
+          }
+        });
+       }
+     }
+   });
+   }
+ });
+
+
+
+ }); 
+
+  $("#btnAsc").click(function() { 
+   loadFilms("year","asc");
+
+ }); 
+  $("#btnDes").click(function() { 
+   loadFilms("year","desc");
+
+ }); 
+  
+
+  $( "#searchFilm" ).keyup(function() {
+   var searchFilm = $('#searchFilm').val();
+   searchFilm = searchFilm.toLowerCase();
+   $.ajax({
+    type: "POST",
+    url: "Controllers/getFilmsController.php",
+    data: { name: searchFilm, searchFilm:"true" },
+    success: function(data){
+
+      $("#result").html(data);
+
+    }
+  });
+
+
+ });
+
+
+}); 
+ function getAllInfo(linkI,nameFilm, yearFilm){
+  var linkImage="";
+  var description="";
+  var director="";
+  $.ajax({
+    type: "POST",
+    url: "Controllers/getAllInfo.php",
+    data: { linkImdb: linkI, getImg:"true" },
+    success: function(data){
+      linkImage=data;
+      
+      $.ajax({
+        type: "POST",
+        url: "Controllers/getAllInfo.php",
+        data: { linkImdb: linkI, getDesc:"true" },
+        success: function(data){
+
+          description=data;
+          
+          $.ajax({
+            type: "POST",
+            url: "Controllers/getAllInfo.php",
+            data: { linkImdb: linkI, getDirect:"true" },
+            success: function(data){
+
+              director=data;
+              
+              
+              console.log("director: "+director+"  descripcion: "+description+"link img: "+linkImage);
+
+              $.ajax({
+                type: "POST",
+                url: "Controllers/getFilmsController.php",
+                data: { name: nameFilm, year: yearFilm, linkImg:linkImage, infoDescription:description, infoDirector:director, updateFilm:"true" },
+                success: function(data){
+
+                 
+
+                }
+              });
+
+            }
+          });
+
+        }
+      });
+
+    }
+  });
+  
+  
+}
+
+$(document).on('click', '.film', function () {
+ var idFilm= $(this).attr("id");
+
+ idFilm=idFilm.substr(4);
+ idFilmNum=parseInt(idFilm);
+ $('body').append('<div id="newDiv"></div>');
+ searchFilmById(idFilm);
+});
+$(document).on('click', '#closeImg', function () {
+  $('div').remove('#newDiv');
+});
+
+function searchFilmById(idFilmNum){
+
+  $.ajax({
+    type: "POST",
+    url: "Controllers/getFilmsController.php",
+    data: { id: idFilmNum, searchFilmById:"true" },
+    success: function(data){
+
+      $("#newDiv").html(data);
+
+    }
+  });
+}
+
+function loadFilms(column,order){
+
+  $.ajax({
+    type: "POST",
+    url: "Controllers/getFilmsController.php",
+    data: { orderBy: column, order: order, getFilms:"true" },
+    success: function(data){
+
+      $("#result").html(data);
+
+    }
+  });
+};
+
